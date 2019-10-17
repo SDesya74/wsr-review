9e2b8a64e6ef45f3e6054ffbce50fa96ccc8ab11
Саня Череп <SDesya74@gmail.com>
2019-10-17 17:46:17 +0300

    Added

---
+++ Misc/Connector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using Java.Net;
+
+namespace WSReview.Misc {
+	static class Connector {
+		private static string Address = @"https://lesson.kristeva.ru/wsr/";
+		private static WebClient Client;
+
+		public static void Init() {
+			Client = new WebClient();
+			Client.Encoding = Encoding.UTF8;
+		}
+
+		public static async Task<string> GetAsync(string method) {
+			try {
+				return await Client.DownloadStringTaskAsync(Address + method);
+			} catch {
+				return null;
+			}
+		}
+
+		public static async Task<string> PostAsync(string method, NameValueCollection data) {
+			try {
+				byte[] bytes = await Client.UploadValuesTaskAsync(Address + method, data);
+				return BytesToString(bytes);
+			} catch {
+				return null;
+			}
+		}
+
+		public static string BytesToString(byte[] bytes) {
+			using(var stream = new MemoryStream(bytes)) {
+				using(var streamReader = new StreamReader(stream)) {
+					return streamReader.ReadToEnd();
+				}
+			}
+		}
+	}
+}
